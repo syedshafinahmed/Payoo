d04d26b2ec8e3311ca8a9b704a315e80e46eb85f
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-22 21:53:28 +0600

    added home

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('loginButton').addEventListener('click', function(e){
+    e.preventDefault();
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+    const pinNumberValue = document.getElementById('pin-number').value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+    if((mobileNumberValueConverted === mobileNumber) && (pinNumberValueConverted === pinNumber)){
+        window.location.href="home.html"
+    }
+    else(
+        alert("Invalid Credentials")
+    )
+})
