e05de2fb333918223c087b0844d98f2e82b76a14
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-23 23:35:40 +0600

    updated cashout feature

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
+// add money feature
 const validPin = 1234;
-document.getElementById("add-money-btn").addEventListener("click", function(e){
+document.getElementById("add-money-btn").addEventListener("click", function (e) {
     e.preventDefault();
     const bank = document.getElementById('bank').value
     const accountNumber = document.getElementById('account-number').value
@@ -9,11 +10,11 @@ document.getElementById("add-money-btn").addEventListener("click", function(e){
 
     const availableBalance = parseInt(document.getElementById("available-balance").innerText)
 
-    if(accountNumber.length < 11){
+    if (accountNumber.length < 11) {
         alert("Please provide a valid Account Number");
         return;
     }
-    if(validPin !== addPin){
+    if (validPin !== addPin) {
         alert("Wrong Pin");
         return;
     }
@@ -22,17 +23,39 @@ document.getElementById("add-money-btn").addEventListener("click", function(e){
 
     document.getElementById("available-balance").innerText = newAvailableBalance
 
-    
+
 })
 
 
-    // toogling feature
-    document.getElementById("add-button").addEventListener("click", function(){
-        document.getElementById("cash-out-parent").style.display = "none"
-        document.getElementById("add-money-parent").style.display = "block"
-    })
+// cashout money feature
+document.getElementById('withdraw-btn').addEventListener("click", function(e){
+    e.preventDefault();
+    const withdrawAmount = parseInt(document.getElementById("withdraw-amount").value)
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+    const updatedBalance = availableBalance - withdrawAmount;
+    document.getElementById("available-balance").innerText = updatedBalance;
 
-    document.getElementById("cash-out-button").addEventListener("click", function(){
-        document.getElementById("add-money-parent").style.display = "none"
-        document.getElementById("cash-out-parent").style.display = "block"
-    })
+    const agentNumber = document.getElementById('agent-number').value
+    if (agentNumber.length < 11) {
+        alert("Please provide a valid Account Number");
+        return;
+    }
+    if (withdrawAmount > availableBalance) {
+        alert("Insufficient Balance");
+        return;
+    }
+})
+
+// toogling feature
+document.getElementById("add-button").addEventListener("click", function () {
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+
+document.getElementById("cash-out-button").addEventListener("click", function () {
+    document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
+})
+
+
+
