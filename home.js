d54d010e9f514ff413006073a3091cfff15a0749
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-23 22:44:08 +0600

    added cashout feature

--- home.js
+++ home.js
@@ -21,5 +21,18 @@ document.getElementById("add-money-btn").addEventListener("click", function(e){
     const newAvailableBalance = availableBalance + addAmount
 
     document.getElementById("available-balance").innerText = newAvailableBalance
+
     
 })
+
+
+    // toogling feature
+    document.getElementById("add-button").addEventListener("click", function(){
+        document.getElementById("cash-out-parent").style.display = "none"
+        document.getElementById("add-money-parent").style.display = "block"
+    })
+
+    document.getElementById("cash-out-button").addEventListener("click", function(){
+        document.getElementById("add-money-parent").style.display = "none"
+        document.getElementById("cash-out-parent").style.display = "block"
+    })
